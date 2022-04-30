0b65c2731e4ee8fb898428e4f48b5d515e695201
luislordev <luislor.dev@gmail.com>
2022-04-30 12:55:03 +0200

    Creado componente fondo y tarjeta de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -6,9 +7,26 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Login Screen'),
-      ),
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 250),
+               CardContainer(
+                 child: Column(
+                   children: [
+                     SizedBox(height:10),
+                     Text('Login', style: Theme.of(context).textTheme.headline4,),
+                     SizedBox(height:30),
+
+                     Text('Formulario')
+                   ],
+                 )
+               )
+            ],
+          ),
+        )
+      )
     );
   }
 }
